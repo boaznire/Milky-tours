13fdf78e70dece60e9299c2fd688bae4623a64e0
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 20:30:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+console.log('React application script is running');
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => (
   <div style={{backgroundColor: 'blue', color: 'white', padding: '20px', margin: '20px'}}>
-    <h1>Milky Tours is loading...</h1>
+    <h1>Milky Tours React App is loaded!</h1>
   </div>
 );
 
