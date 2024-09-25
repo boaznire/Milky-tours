eba3f09d7275eeadf3e9fff5539663238e48d1bf
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 15:53:14 +0300

    Update babel-config.js

--- babel-config.js
+++ babel-config.js
@@ -1,6 +1,6 @@
-{
-  "presets": [
-    "@babel/preset-env",
-    ["@babel/preset-react", {"runtime": "automatic"}]
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }]
   ]
-}
+};
