40843fc06751d2d2df365433882f6166dc25d4cb
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 16:55:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+
+const App = () => <h1>Welcome to Milky Tours</h1>;
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
