bf0755cedfedb13bf386245655172f936a413516
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 21:49:42 +0300

    Update MyComponent.js

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import image1 from '../images/image1.png';
+import image1 from '../Images/image1.png';
+import image2 from '../Images/image2.png';
+import image3 from '../Images/image3.png';
+import image4 from '../Images/image4.png';
 
 function MyComponent() {
   return <img src={image1} alt="Description" />;
