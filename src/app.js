17453bdbee2f4e96a5ee01d5aed12715ef3d2257
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 16:41:21 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ChatBot from './components/ChatBot';
 
 function App() {
   return (
-    <div className="frame-container">
-      {/* Your existing content will go here */}
+    <div>
       <h1>Welcome to Milky Tours</h1>
-      <ChatBot />
     </div>
   );
 }
