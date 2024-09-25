f423cfa184e7a3a6fbc6d854224c49551f30c008
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 16:49:48 +0300

    Update webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -14,12 +14,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
