281e4b76ae744bc956d0f6c68da27b4dd51de40d
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 23:02:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => (
-  <div style={{backgroundColor: 'blue', color: 'white', padding: '20px', margin: '20px'}}>
+  <div style={{ backgroundColor: 'blue', color: 'white', padding: '20px', margin: '20px' }}>
     <h1>Milky Tours React App is loaded!</h1>
   </div>
 );
 
+// React 18+ rendering syntax
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
