4c914165db5d37952d0df6fbe29ee4c6b49700a6
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 21:53:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 console.log('React application script is running');
 
-import './styles/styles.css';
-import './styles/social-media.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import MyComponent from './components/MyComponent';
+import './styles/styles.css';
+import './styles/social-media.css';
 
 const App = () => (
   <div style={{backgroundColor: 'blue', color: 'white', padding: '20px', margin: '20px'}}>
     <h1>Milky Tours React App is loaded!</h1>
+    <MyComponent />
   </div>
 );
 
