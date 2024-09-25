a6ffff4ce818aaa1a831194a38d8b371ca142b6f
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 21:59:20 +0300

    Update webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -18,7 +18,15 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
   },
   plugins: [
