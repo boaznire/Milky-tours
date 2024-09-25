640c8c01e7e792d3881227aa263441d5cf90d017
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 21:48:50 +0300

    Create MyComponent.js

---
+++ src/components/MyComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import image1 from '../images/image1.png';
+
+function MyComponent() {
+  return <img src={image1} alt="Description" />;
+}
+
+export default MyComponent;
