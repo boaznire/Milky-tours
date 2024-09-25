92ab91c4cf0a3a0984c3aa2b1f8140427caa4c99
Boaz Ezra <84347993+boaznire@users.noreply.github.com>
2024-09-25 22:57:16 +0300

    Update webpack-config.js

--- webpack-config.js
+++ webpack-config.js
@@ -1,36 +1,27 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,   // Match both .js and .jsx files
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+          loader: 'babel-loader',   // Use babel-loader for JSX and JS
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']   // Resolve .js and .jsx files
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'), // Where dev server serves files from
+    hot: true   // Enable Hot Module Replacement
+  }
 };
